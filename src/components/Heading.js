5e246d3f3087ee23557adbae1d37b541a92ec180
Shtekata <gesheval@gmail.com>
2021-03-12 15:31:11 +0200

    Update Heading.js

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -1,6 +1,7 @@
 import { testService } from '../services/testService';
 
 const message = testService.testMessage.then(x => x);
+console.log(message.message);
 const Heading = (x) => (
     <header>
         <h1>Hello world</h1>
