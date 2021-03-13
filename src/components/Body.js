e244fd0efdbc736a03bf4408a051389834976e95
Shtekata <gesheval@gmail.com>
2021-03-13 16:34:52 +0200

    Update Body.js

--- src/components/Body.js
+++ src/components/Body.js
@@ -15,8 +15,8 @@ class Body extends Component{
         testService.testMessage.then(x => this.setState({ message: x.message }));
         testService.createEntity({
             title: 'Pesho',
-            description: 'Gosho',
-            solution: 'Gesho',
+            description: 'GoshoGoshoGosho',
+            solution: 'GeshoGeshoGesho',
             isPublic: 'on'
         });
         // testService.register({ username: 'Shtekata', email: 'gesheval@gmail.com', password: '7u87u8', rePassword: '7u87u8' });
